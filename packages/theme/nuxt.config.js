b2475f3ff242e434f67a43bb10e00a6a802d087b
Andriy Vasylytsya <voolve@gmail.com>
2022-03-08 00:20:12 +0200

    (chore): rename default names

--- packages/theme/nuxt.config.js
+++ packages/theme/nuxt.config.js
@@ -50,11 +50,11 @@ export default {
       // @core-development-only-end
       useRawSource: {
         dev: [
-          '@vue-storefront/__replace_me__',
+          '@vue-storefront/wawibox',
           '@vue-storefront/core'
         ],
         prod: [
-          '@vue-storefront/__replace_me__',
+          '@vue-storefront/wawibox',
           '@vue-storefront/core'
         ]
       }
@@ -63,8 +63,8 @@ export default {
     ['@vue-storefront/nuxt-theme', {
       generate: {
         replace: {
-          apiClient: '@vue-storefront/__replace_me__-api',
-          composables: '@vue-storefront/__replace_me__'
+          apiClient: '@vue-storefront/wawibox-api',
+          composables: '@vue-storefront/wawibox'
         }
       }
     }],
@@ -72,7 +72,7 @@ export default {
     /* project-only-start
     ['@vue-storefront/nuxt-theme'],
     project-only-end */
-    ['@vue-storefront/__replace_me__/nuxt', {}]
+    ['@vue-storefront/wawibox/nuxt', {}]
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
